b6bd405f233dbf9295e23b0ab0e6de9eb6cee366
kisielski <92318532+kisielski@users.noreply.github.com>
2023-11-05 00:02:22 +0100

    Create influxdb_write.py

---
+++ influxdb_write.py
@@ -0,0 +1,21 @@
+import influxdb_client, os, time
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+token = "token"
+org = "pgracing"
+url = "rpi.local:8086"
+bucket="telemetry"
+
+write_client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
+write_api = write_client.write_api(write_options=SYNCHRONOUS)
+
+for value in range(5):
+  point = (
+    Point("measurement1")
+    .tag("tagname1", "tagvalue1")
+    .field("field1", value)
+  )
+  write_api.write(bucket=bucket, org=org, record=point)
+  print(point)
+  time.sleep(1)
