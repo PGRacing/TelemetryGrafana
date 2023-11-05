05ba829c4e70f5cc096888fc146574307fd230e6
kisielski <92318532+kisielski@users.noreply.github.com>
2023-11-05 01:18:25 +0100

    config file

---
+++ conf_influxdb.py
@@ -0,0 +1,11 @@
+import influxdb_client, os, time, csv
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+token = "token"
+org = "pgracing"
+url = "rpi.local:8086"
+bucket="telemetry"
+
+write_client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
+write_api = write_client.write_api(write_options=SYNCHRONOUS)
