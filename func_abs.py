1fca73dfdcf670f8cc7d8708cf384060055fbbb8
kisielski <92318532+kisielski@users.noreply.github.com>
2023-11-07 22:31:15 +0100

    cleanup, logs

--- func_abs.py
+++ func_abs.py
@@ -3,19 +3,6 @@ from datetime import datetime
 from conf_influxdb import *
 from utils_timestamp import *
 
-def short_timestamp_to_standard_string(org_timestamp):
-  int_timestamp = int(org_timestamp)
-  minutes = int(int_timestamp / 60000) % 60
-  str_minutes = str(minutes)
-  if minutes < 10:
-    str_minutes = '0' + str_minutes
-  seconds = int(int_timestamp / 1000) % 60
-  str_seconds = str(seconds)
-  if seconds < 10:
-    str_seconds = '0' + str_seconds
-  timestamp = str('T00:' + str_minutes + ':' + str_seconds + '.' + str(int_timestamp % 1000))
-  return timestamp
-
 def import_csv_abs(filepath, start_time):
   # CSV column names as following:
   # timestamp,ID,speed
@@ -28,13 +15,8 @@ def import_csv_abs(filepath, start_time):
 
   for row in csv_reader:
     #print(f'{row["timestamp"]}; {row["ID"]}; {row["speed"]}')
-    #timestamp '2023-11-04T11:00:00.000000Z'
-    #org_timestamp = row["timestamp"]
     # TODO switch timestamp format
-    #timestamp = str(date + 'T' + org_timestamp[:8] + '.' + org_timestamp[9:12] + '000Z')
-    #timestamp = str(date + short_timestamp_to_standard_string(org_timestamp))
     timestamp = start_time_add_millis_timestamp(start_time, row["timestamp"])
-    print(timestamp)
     point = (
       Point('abs')
       .tag("ID", f'{row["ID"]}')
@@ -42,10 +24,10 @@ def import_csv_abs(filepath, start_time):
       .time(timestamp)
     )
     points.append(point)
-    if line_count % 1000 == 0:
-      #write_api.write(bucket=bucket, org=org, record=points)
+    if line_count % 5000 == 0:
+      write_api.write(bucket=bucket, org=org, record=points)
       points.clear()
     line_count += 1
 
   endTime = datetime.datetime.now()
-  print(f'Imported {line_count} rows in {endTime - startTime}')
+  print(f'ABS: Imported {line_count} rows in {endTime - startTime}')
