38b00db5fec417444430b1486ea9a58258d2ac43
kisielski <92318532+kisielski@users.noreply.github.com>
2023-11-07 22:32:36 +0100

    write synchronous for batches

--- conf_influxdb.py
+++ conf_influxdb.py
@@ -8,4 +8,4 @@ url = "rpi.local:8086"
 bucket="telemetry"
 
 write_client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
-write_api = write_client.write_api()
+write_api = write_client.write_api(write_options=SYNCHRONOUS)
