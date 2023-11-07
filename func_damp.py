5fabbbe462cb1d5d9da97cff0741018022de783e
kisielski <92318532+kisielski@users.noreply.github.com>
2023-11-07 22:31:41 +0100

    signature change, cleanup

--- func_damp.py
+++ func_damp.py
@@ -1,8 +1,9 @@
 import csv
 from datetime import datetime
 from conf_influxdb import *
+from utils_timestamp import *
 
-def import_csv_damp(filepath, date):
+def import_csv_damp(filepath, start_time):
   # CSV column names as following:
   # timestamp,ID,delta
   # date like '2023-11-04'
@@ -10,12 +11,11 @@ def import_csv_damp(filepath, date):
   csv_reader = csv.DictReader(file)
   line_count = 0
   points = []
-  startTime = datetime.now()
+  startTime = datetime.datetime.now()
 
   for row in csv_reader:
     #print(f'{row["timestamp"]}; {row["ID"]}; {row["delta"]}')
-    org_timestamp = row["timestamp"]
-    timestamp = str(date + 'T' + org_timestamp[:8] + '.' + org_timestamp[9:12] + '000Z')
+    timestamp = start_time_add_timestamp(start_time, row["timestamp"])
     point = (
       Point('damp')
       .tag("ID", f'{row["ID"]}')
@@ -23,10 +23,10 @@ def import_csv_damp(filepath, date):
       .time(timestamp)
     )
     points.append(point)
-    if line_count % 1000 == 0:
+    if line_count % 5000 == 0:
       write_api.write(bucket=bucket, org=org, record=points)
       points.clear()
     line_count += 1
 
-  endTime = datetime.now()
-  print(f'Imported {line_count} rows in {endTime - startTime}')
+  endTime = datetime.datetime.now()
+  print(f'DAMP: Imported {line_count} rows in {endTime - startTime}')
