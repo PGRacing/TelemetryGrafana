123005f6ad36b9d9f75d5357368c16c8666c971f
Malwina-K <malwina@fastmail.com>
2023-12-13 10:06:06 +0100

    public config

--- conf_influxdb.py
+++ conf_influxdb.py
@@ -2,9 +2,9 @@ import influxdb_client, os, time
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 
-token = "token"
-org = "pgracing"
-url = "http://localhost:8086/"
+token = "lBvDJzs-1cHSKwdqorD4_-xxs2yeg6CgvJcPkZNADNEibLh4xLFEhjZHUIzawXhTA52Xs0MuAPZ1944IMm7Lyw=="
+org = "PGRacingTeam"
+url = "http://34.118.114.249/"
 bucket="telemetry"
 
 write_client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
