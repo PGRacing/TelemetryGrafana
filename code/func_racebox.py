43bdb60038ec5c2421ad1d3940c3da29ccfc197b
Malwina-K <malwina@fastmail.com>
2024-01-01 21:55:32 +0100

    new kalman for GPS

--- code/func_racebox.py
+++ code/func_racebox.py
@@ -2,6 +2,8 @@ import numpy as np
 import csv
 from conf_influxdb import *
 import math
+from filterpy.kalman import KalmanFilter
+from filterpy.common import Q_discrete_white_noise
 
 
 path = 'C:/Users/malwi/Documents/MEGA/PG/PGRacingTeam/telemetry_data/23_11_05_Pszczolki/'
@@ -9,14 +11,186 @@ path = 'C:/Users/malwi/Documents/MEGA/PG/PGRacingTeam/telemetry_data/23_11_05_Ps
 GForce =  9.80665 # m/s2
 timestep = 0.04 # s
 file_counter = 0
+total_rows_calculated = 0
 deg_to_met = 111196.672 
 
 
-def find_acc(filepath):
-    global file_counter
-    for i in range (1, 16):
-        linear_kalman(filepath + 'ACC-' + str(i) + '.csv')
-        file_counter += 1
+def find_racebox(filepath):
+  global file_counter
+  for i in range (1, 16):
+    file_counter += 1
+    kalman(filepath + 'RB-' + str(i) + '.csv')
+    print(f'Import of file number {file_counter} finished')
+  print(f'Successfully calculated {total_rows_calculated} lines!')
+    
+
+def kalman(filefullpath):
+  global total_rows_calculated
+  file = open(filefullpath, 'r')
+  csvreader_object = csv.reader(file)
+  for i in range (1, 13):
+      next(csvreader_object)
+      
+  data = csv.DictReader(file)
+  row_counter = 0
+  points = []
+
+  f = list(range(2))
+  for row in data:
+    signal_loss = False
+    row_counter += 1
+    total_rows_calculated += 1
+    #print(f'File: {file_counter}, Row: {row_counter}')
+
+    row['GForceX'] = float(row['GForceX']) * GForce 
+    row['GForceY'] = float(row['GForceY']) * GForce 
+    row['GForceZ'] = float(row['GForceZ']) * GForce
+
+    row['GyroX'] = float(row['GyroX'])
+    row['GyroY'] = float(row['GyroY'])
+    row['GyroZ'] = float(row['GyroZ'])
+
+    if row['Latitude'] != '' and row['Longitude'] != '' and row['Speed'] != '':
+      row['Latitude'] = float(row['Latitude'])
+      row['Longitude'] = float(row['Longitude'])
+      speed_mps = float(row['Speed']) * (10/36)
+    elif row['Latitude'] == '':
+      row['Latitude'] = 0.0
+      signal_loss = True
+    elif row['Longitude'] != '':
+       row['Longitude'] = 0.0
+       signal_loss = True
+    elif row['Speed'] != '':
+       row['Speed'] = 0.0
+       signal_loss = True
+  
+
+    if row_counter == 1:
+      var = 0.0000001
+      for i in range(2):
+        f[i] = KalmanFilter(dim_x=2, dim_z=1)
+        if i == 0:
+          f[i].x = np.array([[row['Latitude']], 
+                              [row['GyroZ']]])  # initial state (position and velocity)
+        else:
+          f[i].x = np.array([[row['Longitude']], 
+                              [row['GyroZ']]])
+        
+        f[i].F = np.array([[1., timestep], 
+                            [0., 1.]])  # state transition matrix
+        f[i].H = np.array([[1., 0.]])  # Measurement function
+        f[i].P = np.array([[10., 0.], 
+                            [0., 10.]])  # covariance matrix
+        # proces noise and measurement noise needs to be fine tuned
+        # measurement noise smaller -> kalman filter follows raw data more closely
+        # process noise bigger -> kalman filter follows raw data more closely
+        # but this need to be checked
+        f[i].R = np.array([[var]])  # measurement noise
+        f[i].Q = Q_discrete_white_noise(dim=2, dt=timestep, var=var)  # process noise
+    
+    if signal_loss == True:
+      f[0].predict()
+      f[0].update(lat_previous + lat_delta)
+      f[1].predict()
+      f[1].update(lon_previous + lon_delta)
+    else:
+      f[0].predict()
+      f[0].update(row['Latitude'])
+      f[1].predict()
+      f[1].update(row['Longitude'])
+
+    if row_counter >= 2:
+      lat_delta = float(f[0].x[0][0]) - lat_previous
+      lon_delta = float(f[1].x[0][0]) - lon_previous
+
+    lat_previous = float(f[0].x[0][0])
+    lon_previous = float(f[1].x[0][0])
+    
+    # latitude and longitude
+    point = (
+      Point('gps')
+      .tag('ID', 'gps_position')
+      .field('latitude', float(f[0].x[0][0]))
+      .field('longitude', float(f[1].x[0][0]))
+      .time(row['Time'])
+    )
+    points.append(point)
+
+    # speed
+    point = (
+        Point('spd')
+        .tag('Record', "velocity")
+        .field('speed', float(row['Speed']))
+        .time(row['Time'])
+        )
+    points.append(point)
+    
+    # gyro_x
+    point = (
+      Point('gyro')
+      .tag("axis", "x")
+      .field("gyro_x", row["GyroX"])
+      .time(row['Time'])
+    )
+    points.append(point)
+    
+    # gyro_y
+    point = (
+      Point('gyro')
+      .tag("axis", "y")
+      .field("gyro_y", row["GyroY"])
+      .time(row['Time'])
+    )
+    points.append(point)
+    
+    # gyro_z
+    point = (
+      Point('gyro')
+      .tag("axis", "z")
+      .field("gyro_z", row["GyroZ"])
+      .time(row['Time'])
+    )
+    points.append(point)
+    
+    # acc_x
+    point = (
+      Point('acc')
+      .tag("axis", "x")
+      .field("acc_x", row["GForceX"])
+      .time(row['Time'])
+    )
+    points.append(point)
+    
+    # acc_y
+    point = (
+      Point('acc')
+      .tag("axis", "y")
+      .field("acc_y", row["GForceY"])
+      .time(row['Time'])
+    )
+    points.append(point)
+    
+    # acc_z
+    point = (
+      Point('acc')
+      .tag("axis", "z")
+      .field("acc_z", row["GForceZ"])
+      .time(row['Time'])
+    )
+    points.append(point)
+
+    if row_counter % 555 == 0:
+      write_api.write(bucket=bucket, org=org, record=points)
+      points.clear()
+
+  
+
+
+
+
+
+
+
 
 def linear_kalman(filefullpath):
     file = open(filefullpath, 'r')
@@ -222,241 +396,5 @@ def linear_kalman(filefullpath):
       #      velocity_x_old = velocity_x_new
       #      velocity_y_old = velocity_y_new
             
-            
-        
 
-
-def import_acc(filefullpath):
-    global GForce
-    global timestep
-    global conversion_rate
-    global row_counter
-    
-    # [0]Record,[1]Time,[2]Latitude,[3]Longitude,[4]Altitude,[5]Speed,
-    # [6]GForceX,[7]GForceY,[8]GForceZ,[9]Lap,[10]GyroX,[11]GyroY,[12]GyroZ
-    
-    # speed in km/h
-    # GyroX, GyroY, GyroZ in rad/s
-    # GForceX, GForceY, GForceZ in g
-    
-    # p = GyroX, q = GyroY, r = GyroZ
-    
-    # date like 2023-11-05T08:01:59.280Z (date T time Z)
-    file = open(filefullpath, 'r')
-    csvreader_object = csv.reader(file)
-    for i in range (1, 13):
-        next(csvreader_object)
-        
-    data = csv.DictReader(file)
-    
-    # NOT SURE!!!!!!!!!!!!!!!!!!!!
-    variance_phi = 0.01
-    variance_theta = 0.01
-    
-    
-    
-    variance_gps = 0.01  # (10 cm)^2
-    
-    Q = np.array([
-                [variance_phi, 0, 0, 0],
-                [0, variance_theta, 0, 0],
-                [0, 0, variance_gps, 0],
-                [0, 0, 0, variance_gps]
-                ])
-    
-    
- #   P = np.array([[0, 0], [0, 0]])
-     
-    P = np.eye(4)
-    R = np.array([[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]])
-    
-    points = []
-    line_count = 0
-    x_hat_previous = np.array([[0], [0], [0], [0]])
-    
-    
-    for row in data:
-        row_counter += 1
-        
-        if row_counter == 1:
-            x_hat_previous = np.array([[0], [0], [float(row['Latitude'])], [float(row['Longitude'])]])
-
-        
-        p = float(row['GyroX']) #10
-        q = float(row['GyroY']) #11
-        r = float(row['GyroZ']) #12
-        
-
-      #  row['GForceX'] = float(row['GForceX']) * GForce # 6
-        row['GForceY'] = float(row['GForceY']) * GForce # 7
-        row['GForceZ'] = float(row['GForceZ']) * GForce # 8
-        
-        if float(row['GForceX']) > 1:
-            row['GForceX'] = GForce
-        elif float(row['GForceX']) < (-1):
-            row['GForceX'] = GForce * (-1)
-        else:
-            row['GForceX'] = float(row['GForceX']) * GForce # 6
-        
-        # acceleration is now in m/s2 !!!!!!!!!!!!
-            
-        
-        # roll angle estimate, pitch angle estimate
-        # in radians
-        phi_hat_rad = math.atan(row['GForceY'] / row['GForceZ'])
-        print(row['GForceX'])
-        print(row_counter)
-        theta_hat_rad = math.asin(row['GForceX'] / GForce)     
-        
-        # transforming body rates to Euler rates
-        phi_dot_rad = float(row["GyroX"]) * math.tan(theta_hat_rad) * (math.sin(phi_hat_rad) * float(row['GyroY']) + math.cos(phi_hat_rad) * float(row['GyroZ']))
-        theta_dot_rad = math.cos(phi_hat_rad) * float(row['GyroY']) - math.sin(phi_hat_rad) * float(row['GyroZ'])
-        
-        # integrate  Euler rates to get estimate of roll and pitch angles
-        phi_hat_rad = phi_hat_rad + timestep * phi_dot_rad
-        theta_hat_rad = theta_hat_rad + timestep * theta_dot_rad
-
-        # EKF step1: prediction
-        
-        
-        #state estimate
-      #  if row_counter == 1:
-        x_hat = np.array([[phi_hat_rad], [theta_hat_rad], [row['GForceX']], [row['GForceY']]])
-        
-      #  else:
-       #     x_hat = np.array([[phi_hat_rad * timestep], [theta_hat_rad * timestep], [row['GForceX']], [row['GForceY']]])
-
-        # Euler rates
-        f_x_hat_u = [[phi_dot_rad], [theta_dot_rad]]
-
-        f_x_hat_u = [[1 * p + (math.sin(phi_hat_rad) * math.tan(theta_hat_rad)) * q + math.cos(phi_hat_rad) * math.tan(theta_hat_rad) * 3],
-                    [0 * p + math.cos(phi_hat_rad) * q + (-1) * math.sin(phi_hat_rad) * r]]
-
-        phi_hat_rad += (timestep * phi_dot_rad)
-        theta_hat_rad += (timestep * theta_dot_rad)
-
-
- #       A = np.array([[0, (-1) * math.sin(theta_hat_rad) - r * math.cos(theta_hat_rad)],
-  #           [(-1) * q * math.sin(phi_hat_rad) - r * math.cos(phi_hat_rad), 0]])
-        
-  #      A = np.array([
-   #                 [0, (-1) * math.sin(theta_hat_rad) - r * math.cos(theta_hat_rad), 0, 0],
-   #                 [(-1) * q * math.sin(phi_hat_rad) - r * math.cos(phi_hat_rad), 0, 0, 0],
-   #                 [0, 0, 0, (-1) * math.sin(theta_hat_rad) - r * math.cos(theta_hat_rad)],
-   #                 [0, 0, (-1) * q * math.sin(phi_hat_rad) - r * math.cos(phi_hat_rad), 0]
-   #                 ])
-        
-        A = np.array([
-                    [(-1) * math.sin(theta_hat_rad) - r * math.cos(theta_hat_rad), 0, 0, 0],
-                    [0, (-1) * q * math.sin(phi_hat_rad) - r * math.cos(phi_hat_rad), 0, 0],
-                    [0, 0, (-1) * math.sin(theta_hat_rad) - r * math.cos(theta_hat_rad), 0],
-                    [0, 0, 0, (-1) * q * math.sin(phi_hat_rad) - r * math.cos(phi_hat_rad)]
-                    ])
-        
-        # noise
-   #     print(P)
-        if np.isnan(x_hat[0][0]):
-            print(row_counter)
-            print('NOT 1')
-            break
-            
-    
-        
-   #     Q = np.array([[0, 0], [0, 0]])
-        
-
-        # Perform matrix multiplication and transposition
-     #   print(row_counter)
-  #      print(P)
-        
-        result = P + timestep * (A @ P + P @ A.T + Q)
-        P = result
-        
-        # step2: update
-        
-        sp = math.sin(phi_hat_rad)
-        cp = math.cos(phi_hat_rad)
-        st = math.sin(theta_hat_rad)
-        ct = math.cos(theta_hat_rad)
-        
-        h = np.array([[GForce * st], [(-1.0) * GForce * ct * sp], [(-1.0) * GForce * ct * cp], [x_hat_previous[2][0]], [x_hat_previous[3][0]]])
-#        h = np.array([GForce * st, (-1.0) * GForce * ct * sp, (-1.0) * GForce * ct * cp])
-  #      y = np.array([float(row['GForceX']), float(row['GForceY']), float(row['GForceZ'])])
-        
-        y = np.array([
-                    row['GForceX'],
-                    row['GForceY'],
-                    row['GForceZ'],
-           #         phi_hat_rad,  
-            #        theta_hat_rad,  
-                    float(row['Latitude']),  
-                    float(row['Longitude'])  
-                    ])
-        
-        
-        # Jacobian of h(x, u)
-#        C = np.array([[0.0, GForce * ct], [(-1.0) * GForce * cp * ct, GForce * sp * st], [GForce * sp * ct, GForce * cp * st]])
-
- #       C = np.array([
- #                   [0.0, GForce * ct, 0, 0],
- #                   [(-1.0) * GForce * cp * ct, GForce * sp * st, 0, 0],
- #                   [GForce * sp * ct, GForce * cp * st, 0, 0],
- #                   [0, 0, 1, 0],
- #                   [0, 0, 0, 1]
- #                   ])
-        C = np.array([
-                    [ GForce * ct, 0, 0, 0],
-                    [0, (-1.0) * GForce * cp * ct, GForce * sp * st, 0, 0],
-                    [0, 0, GForce * sp * ct, GForce * cp * st, 0],
-                    [0, 0, 0, 1],
-                    ])
-
- #       print(C)
-   #     print(P)
-   #     print(C @ P @ C.T + R)
-
-        # Calculate Kalman gain
-        K = P @ C.T @ np.linalg.inv(C @ P @ C.T + R)
-        
-        print(K)
-        print(y - h)
-
- #       if row_counter == 1:
- #           x_hat = K @ (y - h)
- #       else:
-        x_hat = x_hat_previous + K @ (y - h)
-        
-        print(row_counter)
-        
-        x_hat_previous = x_hat
-
-        P = (np.eye(4) - K @ C) @ P
-        
-        latitude_correction = x_hat[2][0]
-        longitude_correction = x_hat[3][0]
-        
-#        print(x_hat)
-        
-     #   print(latitude_correction)
-
-        # MEASUREMENT UPDATE
-      
-#        latitude = float(row['Latitude'])
- #       longitude = float(row['Longitude'])
-        
-    #    H = np.concatenate((np.eye(2), np.zeros((2, 2))))
- #       measurement = np.array([latitude, longitude])
-       # x_hat = x_hat.reshape((2, 1))
-       
-
-#       print(C @ x_hat)
-       
- #       innovation = measurement - C @ x_hat
- #       x_hat_ = x_hat + K @ np.concatenate((innovation, np.zeros((2, 1))))
-
-#        fused_states.append(x_hat_)
-                            
- 
-
-
-find_acc(path)
+find_racebox(path)
