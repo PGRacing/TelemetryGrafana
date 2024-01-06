9a7b71e555db0e8fab3e2612ad48644767ade493
Malwina-K <malwina@fastmail.com>
2024-01-06 23:47:08 +0100

    time correction

--- code/func_racebox.py
+++ code/func_racebox.py
@@ -2,6 +2,7 @@ import numpy as np
 import csv
 from conf_influxdb import *
 import math
+from datetime import datetime
 from filterpy.kalman import KalmanFilter
 from filterpy.common import Q_discrete_white_noise
 
@@ -12,15 +13,20 @@ GForce =  9.80665 # m/s2
 timestep = 0.04 # s
 file_counter = 0
 total_rows_calculated = 0
-deg_to_met = 111196.672 
+
+# to be set every track change
+approx_lon = 18.712
+approx_lat = 54.178
+
+conv_rate_lon = math.cos(approx_lat) * (40075000/360) # deg to met
+conv_rate_lat = 111111.0
 
 
 def find_racebox(filepath):
   global file_counter
   for i in range (1, 16):
     file_counter += 1
-    kalman(filepath + 'RB-' + str(i) + '.csv')
-    print(f'Import of file number {file_counter} finished')
+    kalman_one_value(filepath + 'RB-' + str(i) + '.csv')
   print(f'Successfully calculated {total_rows_calculated} lines!')
     
 
@@ -34,12 +40,182 @@ def kalman(filefullpath):
   data = csv.DictReader(file)
   row_counter = 0
   points = []
+  startTime = datetime.now()
+
+  f = KalmanFilter(dim_x=4, dim_z=2)
+  for row in data:
+    signal_loss = False
+    row_counter += 1
+    total_rows_calculated += 1
+
+    timestamp = time_correction(row['Time'])
+
+    row['GForceX'] = float(row['GForceX']) * GForce 
+    row['GForceY'] = float(row['GForceY']) * GForce 
+    row['GForceZ'] = float(row['GForceZ']) * GForce
+
+    row['GyroX'] = float(row['GyroX'])
+    row['GyroY'] = float(row['GyroY'])
+    row['GyroZ'] = float(row['GyroZ'])
+
+    if row['Latitude'] != '' and row['Longitude'] != '':
+      row['Latitude'] = float(row['Latitude'])
+      row['Longitude'] = float(row['Longitude'])
+
+    elif row['Latitude'] == '':
+      row['Latitude'] = 0.0
+      signal_loss = True
+    elif row['Longitude'] != '':
+      row['Longitude'] = 0.0
+      signal_loss = True
+  
+
+    if row_counter == 1:
+      var = 10
+      f.x = np.array([[row['Latitude']*conv_rate_lat],
+                      [row['Longitude']*conv_rate_lon],
+                      [0.],
+                      [0.]])  # initial state (position and velocity)
+        
+      f.F = np.array([[1., 0., timestep, 0.], 
+                      [0., 1., 0., timestep],
+                      [0., 0., 1., 0.],
+                      [0., 0., 0., 1.]])  # state transition matrix
+      f.H = np.array([[1., 0., 0., 0.],
+                      [0., 1., 0., 0.]])  # Measurement function
+      f.P = np.array([[10., 0., 0., 0.], 
+                      [0., 10., 0., 0.],
+                      [0., 0., 10., 0.],
+                      [0., 0., 0., 10.]])  # covariance matrix
+
+      f.R = np.array([[var**2, 0.],
+                      [0., var**2]])  # measurement noise
+
+      f.Q = Q_discrete_white_noise(dim=4, dt=timestep, var=var)  # process noise
+    
+    if signal_loss == True:
+      f.predict()
+    else:
+      f.predict()
+      f.update(np.array([[row['Latitude']*conv_rate_lat],
+                         [row['Longitude']*conv_rate_lon]]))
+    
+    # latitude and longitude
+    point = (
+      Point('gps')
+      .tag('ID', 'gps_position')
+      .field('latitude', float(f.x[0][0])*(1/conv_rate_lat))
+      .field('longitude', float(f.x[1][0])*(1/conv_rate_lon))
+      .time(timestamp)
+    )
+    points.append(point)
+
+    # speed
+    point = (
+        Point('spd')
+        .tag('Record', "velocity")
+        .field('speed', float(row['Speed']))
+        .time(timestamp)
+        )
+    points.append(point)
+
+    point = (
+        Point('spd')
+        .tag('Record', "velocity_x")
+        .field('speed_x', float(f.x[2][0]))
+        .time(timestamp)
+        )
+    points.append(point)
+
+    point = (
+        Point('spd')
+        .tag('Record', "velocity_y")
+        .field('speed_y', float(f.x[3][0]))
+        .time(timestamp)
+        )
+    points.append(point)
+    
+    # gyro_x
+    point = (
+      Point('gyro')
+      .tag("axis", "x")
+      .field("gyro_x", row["GyroX"])
+      .time(timestamp)
+    )
+    points.append(point)
+    
+    # gyro_y
+    point = (
+      Point('gyro')
+      .tag("axis", "y")
+      .field("gyro_y", row["GyroY"])
+      .time(timestamp)
+    )
+    points.append(point)
+    
+    # gyro_z
+    point = (
+      Point('gyro')
+      .tag("axis", "z")
+      .field("gyro_z", row["GyroZ"])
+      .time(timestamp)
+    )
+    points.append(point)
+    
+    # acc_x
+    point = (
+      Point('acc')
+      .tag("axis", "x")
+      .field("acc_x", row["GForceX"])
+      .time(timestamp)
+    )
+    points.append(point)
+    
+    # acc_y
+    point = (
+      Point('acc')
+      .tag("axis", "y")
+      .field("acc_y", row["GForceY"])
+      .time(timestamp)
+    )
+    points.append(point)
+    
+    # acc_z
+    point = (
+      Point('acc')
+      .tag("axis", "z")
+      .field("acc_z", row["GForceZ"])
+      .time(timestamp)
+    )
+    points.append(point)
+
+    if row_counter % 555 == 0:
+      write_api.write(bucket=bucket, org=org, record=points)
+      points.clear()
+  
+  endTime = datetime.now()
+  print(f'Import of file number {file_counter} finished in {endTime - startTime}')
+
+  
+def kalman_one_value(filefullpath):
+  global total_rows_calculated
+  file = open(filefullpath, 'r')
+  csvreader_object = csv.reader(file)
+  for i in range (1, 13):
+      next(csvreader_object)
+      
+  data = csv.DictReader(file)
+  row_counter = 0
+  points = []
+
+  startTime = datetime.now()
 
   f = list(range(2))
   for row in data:
     signal_loss = False
     row_counter += 1
     total_rows_calculated += 1
+    timestamp = time_correction(row['Time'])
     #print(f'File: {file_counter}, Row: {row_counter}')
 
     row['GForceX'] = float(row['GForceX']) * GForce 
@@ -50,18 +226,14 @@ def kalman(filefullpath):
     row['GyroY'] = float(row['GyroY'])
     row['GyroZ'] = float(row['GyroZ'])
 
-    if row['Latitude'] != '' and row['Longitude'] != '' and row['Speed'] != '':
+    if row['Latitude'] != '' and row['Longitude'] != '':
       row['Latitude'] = float(row['Latitude'])
       row['Longitude'] = float(row['Longitude'])
-      speed_mps = float(row['Speed']) * (10/36)
     elif row['Latitude'] == '':
       row['Latitude'] = 0.0
       signal_loss = True
     elif row['Longitude'] != '':
        row['Longitude'] = 0.0
-       signal_loss = True
-    elif row['Speed'] != '':
-       row['Speed'] = 0.0
        signal_loss = True
   
 
@@ -71,10 +243,10 @@ def kalman(filefullpath):
         f[i] = KalmanFilter(dim_x=2, dim_z=1)
         if i == 0:
           f[i].x = np.array([[row['Latitude']], 
-                              [row['GyroZ']]])  # initial state (position and velocity)
+                              [0.]])  # initial state (position and velocity)
         else:
           f[i].x = np.array([[row['Longitude']], 
-                              [row['GyroZ']]])
+                              [0.]])
         
         f[i].F = np.array([[1., timestep], 
                             [0., 1.]])  # state transition matrix
@@ -90,22 +262,13 @@ def kalman(filefullpath):
     
     if signal_loss == True:
       f[0].predict()
-      f[0].update(lat_previous + lat_delta)
       f[1].predict()
-      f[1].update(lon_previous + lon_delta)
     else:
       f[0].predict()
       f[0].update(row['Latitude'])
       f[1].predict()
       f[1].update(row['Longitude'])
 
-    if row_counter >= 2:
-      lat_delta = float(f[0].x[0][0]) - lat_previous
-      lon_delta = float(f[1].x[0][0]) - lon_previous
-
-    lat_previous = float(f[0].x[0][0])
-    lon_previous = float(f[1].x[0][0])
-    
     # latitude and longitude
     point = (
       Point('gps')
@@ -121,7 +284,7 @@ def kalman(filefullpath):
         Point('spd')
         .tag('Record', "velocity")
         .field('speed', float(row['Speed']))
-        .time(row['Time'])
+        .time(timestamp)
         )
     points.append(point)
     
@@ -130,7 +293,7 @@ def kalman(filefullpath):
       Point('gyro')
       .tag("axis", "x")
       .field("gyro_x", row["GyroX"])
-      .time(row['Time'])
+      .time(timestamp)
     )
     points.append(point)
     
@@ -139,7 +302,7 @@ def kalman(filefullpath):
       Point('gyro')
       .tag("axis", "y")
       .field("gyro_y", row["GyroY"])
-      .time(row['Time'])
+      .time(timestamp)
     )
     points.append(point)
     
@@ -148,7 +311,7 @@ def kalman(filefullpath):
       Point('gyro')
       .tag("axis", "z")
       .field("gyro_z", row["GyroZ"])
-      .time(row['Time'])
+      .time(timestamp)
     )
     points.append(point)
     
@@ -157,7 +320,7 @@ def kalman(filefullpath):
       Point('acc')
       .tag("axis", "x")
       .field("acc_x", row["GForceX"])
-      .time(row['Time'])
+      .time(timestamp)
     )
     points.append(point)
     
@@ -166,7 +329,7 @@ def kalman(filefullpath):
       Point('acc')
       .tag("axis", "y")
       .field("acc_y", row["GForceY"])
-      .time(row['Time'])
+      .time(timestamp)
     )
     points.append(point)
     
@@ -175,7 +338,7 @@ def kalman(filefullpath):
       Point('acc')
       .tag("axis", "z")
       .field("acc_z", row["GForceZ"])
-      .time(row['Time'])
+      .time(timestamp)
     )
     points.append(point)
 
@@ -183,11 +346,22 @@ def kalman(filefullpath):
       write_api.write(bucket=bucket, org=org, record=points)
       points.clear()
 
-  
+  endTime = datetime.now()
+  print(f'Import of file number {file_counter} finished in {endTime - startTime}')
 
 
-
-
+def time_correction(row):
+  day = row[:11]
+  hour = int(row[11:13])
+  minutes = int(row[14:16])
+  sec = float(row[17:23]) + 12.3
+  if sec >= 60.:
+     minutes += 1
+     sec -= 60.
+  if minutes >= 60:
+     hour += 1
+     minutes -= 60
+  return day + str(format(hour, "02d")) + ':' + str(format(minutes, "02d")) + ':' + str(format(sec, ".3f")) + 'Z'
 
 
 
