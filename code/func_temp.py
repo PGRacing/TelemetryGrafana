ecb498f7fabba782d47bdfdceeca7285add025c6
Malwina-K <malwina@fastmail.com>
2024-02-26 15:22:43 +0100

    very fast commit

--- code/func_temp.py
+++ code/func_temp.py
@@ -19,78 +19,77 @@ def find_file(filepath):
 
 
 def open_file(filefullpath):
+    file = open(filefullpath, "r")
+    csv_reader = csv.DictReader(file)
     points = []
-    with open(filefullpath, 'r') as file:
-
-        data = csv.DictReader(file)
         
-        #startTime = datetime.now()
-        row_counter = 0
+    #startTime = datetime.now()
+    row_counter = 0
 
-        for row in data:
-            #print(datetime.datetime.fromtimestamp(float(row['timestamp'])))
-            try:
-                timestamp = cooling_system_timestamp(str(datetime.datetime.fromtimestamp(float(row['timestamp']))))
-                float(row['engine_out'])
-                float(row['engine_in'])
-                float(row['radiator_l_in'])
-                float(row['radiator_l_out'])
-                float(row['radiator_r_in'])
-                float(row['radiator_r_out'])
-            except (ValueError, TypeError) as e:
-                #print(e)
-                continue
+    for row in csv_reader:
+        #print(datetime.datetime.fromtimestamp(float(row['timestamp'])))
+        try:
+            timestamp = cooling_system_timestamp(str(datetime.datetime.fromtimestamp(float(row['timestamp']))))
+            float(row['engine_out'])
+            float(row['engine_in'])
+            float(row['radiator_l_in'])
+            float(row['radiator_l_out'])
+            float(row['radiator_r_in'])
+            float(row['radiator_r_out'])
+        except (ValueError, TypeError) as e:
+            #print(e)
+            continue
 
-            point = (
-                Point('temp')
-                .tag("temperature", 'engine')
-                .field("out", float(row['engine_out']))
-                .time(timestamp)
-            )
-            points.append(point)
-            point = (
-                Point('temp')
-                .tag("temperature", 'engine')
-                .field("in", float(row['engine_in']))
-                .time(timestamp)
-            )
-            points.append(point)
-            point = (
-                Point('temp')
-                .tag("temperature", 'radiator_l')
-                .field("in", float(row['radiator_l_in']))
-                .time(timestamp)
-            )
-            points.append(point)
-            point = (
-                Point('temp')
-                .tag("temperature", 'radiator_l')
-                .field("out", float(row['radiator_l_out']))
-                .time(timestamp)
-            )
-            points.append(point)
-            point = (
-                Point('temp')
-                .tag("temperature", 'radiator_r')
-                .field("in", float(row['radiator_r_in']))
-                .time(timestamp)
-            )
-            points.append(point)
-            point = (
-                Point('temp')
-                .tag("temperature", 'radiator_r')
-                .field("out", float(row['radiator_r_out']))
-                .time(timestamp)
-            )
-            points.append(point)
+        point = (
+            Point('temp')
+            .tag("temperature", 'engine')
+            .field("out", float(row['engine_out']))
+            .time(timestamp)
+        )
+        points.append(point)
+        point = (
+            Point('temp')
+            .tag("temperature", 'engine')
+            .field("in", float(row['engine_in']))
+            .time(timestamp)
+        )
+        points.append(point)
+        point = (
+            Point('temp')
+            .tag("temperature", 'radiator_l')
+            .field("in", float(row['radiator_l_in']))
+            .time(timestamp)
+        )
+        points.append(point)
+        point = (
+            Point('temp')
+            .tag("temperature", 'radiator_l')
+            .field("out", float(row['radiator_l_out']))
+            .time(timestamp)
+        )
+        points.append(point)
+        point = (
+            Point('temp')
+            .tag("temperature", 'radiator_r')
+            .field("in", float(row['radiator_r_in']))
+            .time(timestamp)
+        )
+        points.append(point)
+        point = (
+            Point('temp')
+            .tag("temperature", 'radiator_r')
+            .field("out", float(row['radiator_r_out']))
+            .time(timestamp)
+        )
+        points.append(point)
 
-            if row_counter % 100 == 0:
-                write_api.write(bucket=bucket, org=org, record=points)
-                points.clear()
-            row_counter += 1
+        if row_counter % 100 == 0:
+            write_api.write(bucket=bucket, org=org, record=points)
+            points.clear()
+        row_counter += 1
 
-        write_api.write(bucket=bucket, org=org, record=points)
-        #endTime = datetime.datetime.now()
-        print(f'Calculated file number {file_counter} in')
+    write_api.write(bucket=bucket, org=org, record=points)
+    #endTime = datetime.datetime.now()
+    print(f'Calculated file number {file_counter} in')
 
 find_file(path)
