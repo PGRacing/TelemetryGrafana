aa3705cfe907fb761add7acd440b3462dc580003
Malwina796 <malwina@fastmail.com>
2024-03-07 00:39:08 +0100

    add time loss and some minor changes

--- code/func_racebox.py
+++ code/func_racebox.py
@@ -10,7 +10,7 @@ from filterpy.kalman import KalmanFilter
 from filterpy.common import Q_discrete_white_noise
 
 
-path = 'C:/Users/malwi/Documents/MEGA/PGRacingTeam/000 telemetry_data/23_11_05_Pszczolki/'
+path = 'C:/Users/malwi/Documents/MEGA/PGRacingTeam/000 telemetry_data/24_03_03_Pszczolki/racebox/'
 
 GFORCE =  9.80665 # m/s2
 TIMESTEP = 0.04 # s
@@ -41,9 +41,12 @@ def find_racebox(filepath):
     global var_gps
 
     startProgram = datetime.now()
-    for i in range (1, 14):
-        file_counter += 1
-        open_file(filepath + 'RB-' + str(i) + '.csv')
+    for item in os.listdir(path):
+        full_path = os.path.join(path, item)
+
+        if os.path.isfile(full_path) and item.endswith('.csv'):
+            file_counter += 1
+            open_file(full_path)
     endProgram = datetime.now()
     print(f'Successfully imported {file_counter} files in {endProgram - startProgram}!')
 
@@ -57,10 +60,6 @@ def open_file(filefullpath):
     global special_lap_acc
     
     with open(filefullpath, 'r') as file:
-        csvreader_object = csv.reader(file)
-        for i in range (1, 13):
-            next(csvreader_object)
-            
         lap_timer = LapTimer()
         data = csv.DictReader(file)
         points = []
@@ -347,353 +346,10 @@ def open_file(filefullpath):
 
 
 def wrap_angle(angle):
-    #return (angle + 180) % 360 - 180
     while angle > 360:
         angle -= 360
     while angle < 0:
         angle+=360
     return angle
    
-   
-
-
-def kalman(filefullpath):
-  file = open(filefullpath, 'r')
-  csvreader_object = csv.reader(file)
-  for i in range (1, 13):
-      next(csvreader_object)
-      
-  data = csv.DictReader(file)
-  row_counter = 0
-  points = []
-  startTime = datetime.now()
-
-  f = KalmanFilter(dim_x=4, dim_z=2)
-  for row in data:
-    signal_loss = False
-    row_counter += 1
-    total_rows_calculated += 1
-
-    #timestamp = time_correction(row['Time'])
-    timestamp = row['Time']
-
-    
-
-    if row['Latitude'] != '' and row['Longitude'] != '':
-      row['Latitude'] = float(row['Latitude'])
-      row['Longitude'] = float(row['Longitude'])
-
-    elif row['Latitude'] == '':
-      row['Latitude'] = 0.0
-      signal_loss = True
-    elif row['Longitude'] != '':
-      row['Longitude'] = 0.0
-      signal_loss = True
-  
-
-    if row_counter == 1:
-      var = 10
-      f.x = np.array([[row['Latitude']*conv_rate_lat],
-                      [row['Longitude']*conv_rate_lon],
-                      [0.],
-                      [0.]])  # initial state (position and velocity)
-        
-      f.F = np.array([[1., 0., timestep, 0.], 
-                      [0., 1., 0., timestep],
-                      [0., 0., 1., 0.],
-                      [0., 0., 0., 1.]])  # state transition matrix
-      f.H = np.array([[1., 0., 0., 0.],
-                      [0., 1., 0., 0.]])  # Measurement function
-      f.P = np.array([[10., 0., 0., 0.], 
-                      [0., 10., 0., 0.],
-                      [0., 0., 10., 0.],
-                      [0., 0., 0., 10.]])  # covariance matrix
-
-      f.R = np.array([[var**2, 0.],
-                      [0., var**2]])  # measurement noise
-
-      f.Q = Q_discrete_white_noise(dim=4, dt=timestep, var=var)  # process noise
-    
-    if signal_loss == True:
-      f.predict()
-    else:
-      f.predict()
-      f.update(np.array([[row['Latitude']*conv_rate_lat],
-                         [row['Longitude']*conv_rate_lon]]))
-    
-    # latitude and longitude
-    point = (
-      Point('gps')
-      .tag('ID', 'gps_position')
-      .field('latitude', float(f.x[1][0])*(1/conv_rate_lat))
-      .field('longitude', float(f.x[0][0])*(1/conv_rate_lon))
-      .time(timestamp)
-    )
-    points.append(point)
-
-    # speed
-    point = (
-        Point('spd')
-        .tag('Record', "velocity")
-        .field('speed', float(row['Speed']))
-        .time(timestamp)
-        )
-    points.append(point)
-
-    point = (
-        Point('spd')
-        .tag('Record', "velocity_x")
-        .field('speed_x', float(f.x[2][0]))
-        .time(timestamp)
-        )
-    points.append(point)
-
-    point = (
-        Point('spd')
-        .tag('Record', "velocity_y")
-        .field('speed_y', float(f.x[3][0]))
-        .time(timestamp)
-        )
-    points.append(point)
-    
-    # acc_x
-    point = (
-      Point('acc')
-      .tag("axis", "x")
-      .field("acc_x", row["GForceX"])
-      .time(timestamp)
-    )
-    points.append(point)
-    
-    # acc_y
-    point = (
-      Point('acc')
-      .tag("axis", "y")
-      .field("acc_y", row["GForceY"])
-      .time(timestamp)
-    )
-    points.append(point)
-    
-    # acc_z
-    point = (
-      Point('acc')
-      .tag("axis", "z")
-      .field("acc_z", row["GForceZ"])
-      .time(timestamp)
-    )
-    points.append(point)
-
-    if row_counter % 555 == 0:
-      write_api.write(bucket=bucket, org=org, record=points)
-      points.clear()
-  
-  endTime = datetime.now()
-  print(f'GPS: import finished in {endTime - startTime}')
-
-
-def linear_kalman(filefullpath):
-    file = open(filefullpath, 'r')
-    csvreader_object = csv.reader(file)
-    for i in range (1, 13):
-        next(csvreader_object)
-        
-    data = csv.DictReader(file)
-    row_counter = 0
-   
-    points = []
-    line_count = 0
-    
-    angular_position = math.radians(90)
-    
-    C = np.array([[1, 0, 0, 0],
-                  [0, 1, 0, 0]])
-    
-    A = np.array([[1, 0, timestep, 0],
-                  [0, 1, 0, timestep],
-                  [0, 0, 1, 0],
-                  [0, 0, 0, 1]])
-    
-
-    latitude_new = 0
-    longitude_new = 0
-    
-    x_old = np.array([[0],
-                      [0],
-                      [0],
-                      [0]])
-    longitude_old = 0
-    latitude_old = 0
-    
-    
-    for row in data:
-        row_counter += 1
-        line_count += 1
-        
-        
-        print(f'Row: {row_counter}')
-        
-        
-        row['GForceX'] = float(row['GForceX']) * GForce 
-        row['GForceY'] = float(row['GForceY']) * GForce 
-        row['GForceZ'] = float(row['GForceZ']) * GForce
-        
-        row['GyroX'] = float(row['GyroX'])
-        row['GyroY'] = float(row['GyroY'])
-        row['GyroZ'] = float(row['GyroZ'])
-        
-        if ((row['Latitude'] != '') and (row['Longitude'] != '')):
-            row['Latitude'] = float(row['Latitude']) # y
-            row['Longitude'] = float(row['Longitude']) # x
-                
-        
-        if row_counter == 1:
-
-            longitude_old = row['Longitude']
-            latitude_old = row['Latitude']
-            velocity_x = row['GForceX'] * timestep
-            velocity_y = row['GForceY'] * timestep
-            
-            x_old = np.array([[longitude_old],
-                              [latitude_old],
-                              [velocity_x],
-                              [velocity_y]])
-        
-        
-        
-        if row_counter > 1:
-            angular_position_delta = (row['GyroZ'] * timestep)/10  + 0.5 * row['GForceZ'] * (timestep**2)
-            angular_position += angular_position_delta
-            
-        #    angular_acceleration = angular_position_delta / timestep
-            
-        #    print(math.degrees(angular_position))
-
-        #    velocity_x_delta = (row['GyroX'] * math.cos(angular_position) + row['GForceX'] * timestep)
-        #    velocity_y_delta = row['GyroY'] * math.cos(angular_position) + row['GForceY'] * timestep
-            
-            velocity_x_delta = row['GForceX'] * timestep
-            velocity_y_delta = row['GForceY'] * timestep 
-            
-            velocity_x += velocity_x_delta
-            velocity_y += velocity_y_delta
-            
-            x_old = np.array([[longitude_old],
-                              [latitude_old],
-                              [velocity_x],
-                              [velocity_y]])
-
-            x_new = A @ x_old
-           
-            y = C @ x_new
-           
-            latitude_new = latitude_old + (velocity_y * timestep)/deg_to_met
-            longitude_new = longitude_old + (velocity_x * timestep)/(deg_to_met) #* math.cos(math.radians(latitude_new)))
-            
-            
-            if ((row['Longitude'] == '') or (row['Latitude'] == '')):
-                longitude_correction = longitude_new #float(y[0][0])
-                latitude_correction = latitude_new  #float(y[1][0])
-            else:
-                longitude_correction = (longitude_new + row['Longitude']) / 2
-                latitude_correction = (latitude_new + row['Latitude']) / 2
-                
-
-            longitude_new = longitude_correction
-            latitude_new = latitude_correction
-           
-    #       if latitude_correction == 0:
-    #           print(row['Record'])
-    #           break
-    #       velocity_x_old = velocity_x_new
-    #        velocity_y_old = velocity_y_new
-            x_old = x_new
-            longitude_old = longitude_new
-            latitude_old = latitude_new
-           
-            #latitude, longitude
-            point = (
-              Point('gps')
-              .tag('ID', 'gps_position')
-              .field('latitude', latitude_correction)
-              .field('longitude', longitude_correction)
-              .time(row['Time'])
-            )
-            points.append(point)
-            
-            #speed
-            point = (
-                Point('spd')
-                .tag('Record', "velocity")
-                .field('speed', float(row['Speed']))
-                .time(row['Time'])
-                )
-            points.append(point)
-            
-            # gyro_x
-            point = (
-              Point('gyro')
-              .tag("axis", "x")
-              .field("gyro_x", float(row["GyroX"]))
-              .time(row['Time'])
-            )
-            points.append(point)
-            
-            # gyro_y
-            point = (
-              Point('gyro')
-              .tag("axis", "y")
-              .field("gyro_y", float(row["GyroY"]))
-              .time(row['Time'])
-            )
-            points.append(point)
-            
-            # gyro_z
-            point = (
-              Point('gyro')
-              .tag("axis", "z")
-              .field("gyro_z", float(row["GyroZ"]))
-              .time(row['Time'])
-            )
-            points.append(point)
-            
-            # acc_x
-            point = (
-              Point('acc')
-              .tag("axis", "x")
-              .field("acc_x", float(row["GForceX"]))
-              .time(row['Time'])
-            )
-            points.append(point)
-            
-            # acc_y
-            point = (
-              Point('acc')
-              .tag("axis", "y")
-              .field("acc_y", float(row["GForceY"]))
-              .time(row['Time'])
-            )
-            points.append(point)
-            
-            # acc_z
-            point = (
-              Point('acc')
-              .tag("axis", "z")
-              .field("acc_z", float(row["GForceZ"]))
-              .time(row['Time'])
-            )
-            points.append(point)
-           
-   
-            if row_counter % 555 == 0:
-              write_api.write(bucket=bucket, org=org, record=points)
-              points.clear()
-            line_count += 1
-
-            
-            
-       #     angular_position_old = angular_position_new
-      #      velocity_x_old = velocity_x_new
-      #      velocity_y_old = velocity_y_new
-            
-
 find_racebox(path)
--- code/time_loss.py
+++ code/time_loss.py
@@ -1,7 +1,15 @@
+from math import sqrt, pow, cos
 
+approx_lon = 18.712
+approx_lat = 54.178
+lat_prev = 54.1784441
+lon_prev = 18.7133100
 
-class PointDetails:
-    def __init__(self, x, y, timestamp) -> None:
+conv_rate_lon = cos(approx_lat) * (40075000/360) # deg to met
+conv_rate_lat = 111111.0
+
+class TrackPoint:
+    def __init__(self, x:float, y:float, timestamp, distance:float, ) -> None:
         self.x = x
         self.y = y
         self.timestamp = timestamp
@@ -14,15 +22,121 @@ class TimeLoss:
         self.current_x = 0
         self.current_y = 0
         self.current_timestamp = 0
+        self.delta = 0
+        self.last_used_point = None
         self.best_lap = []
     
-    def recordFirstLap(x, y, timestamp):
-        firstPointTimestap = 0.
-        isLapfinished = 0
-        self.best_lap
+    def recordFirstLap(self, x:float, y:float, timestamp):
+        current_point = TrackPoint(x, y, timestamp)
+        self.best_lap.append(current_point)
+    
+    def calucate_gain_loss(self, x:float, y:float, timestamp):
+        if self.last_used_point == None:
+            self.delta = 0
+        
+
+    def calc_distance(self, current_x, current_y, current_timestamp):
+        def dist(x1: float, y1: float, x2: float, y2: float) -> float:
+            return sqrt(pow((x1 - x2)*conv_rate_lon, 2) + pow((y1 - y2)*conv_rate_lat, 2))
+        current_point = TrackPoint(current_x, current_y, current_timestamp)
+        # finding 3 nearest points
+        for i in range (len(self.best_lap)):
+            if i == 0:
+                nearest_point = self.best_lap[i]
+            elif i == 1:
+                if dist(current_x, current_y, self.best_lap[0].x, self.best_lap[0].y) <\
+                dist(current_x, current_y, self.best_lap[1].x, self.best_lap[1].y):
+                    nearest_point = self.best_lap[0]
+                    second_nearest_point = self.best_lap[1]
+                else:
+                    nearest_point = self.best_lap[1]
+                    second_nearest_point = self.best_lap[0]
+            elif i == 2:
+                if dist(current_x, current_y, self.best_lap[2].x, self.best_lap[2].y) <\
+                dist(current_x, current_y, nearest_point.x, nearest_point.y):
+                    third_nearest_point = second_nearest_point
+                    second_nearest_point = nearest_point
+                    nearest_point = self.best_lap[2]
+                elif dist(current_x, current_y, self.best_lap[2].x, self.best_lap[2].y) >\
+                dist(current_x, current_y, second_nearest_point.x, second_nearest_point.y):
+                    third_nearest_point = self.best_lap[2]
+                else:
+                    third_nearest_point = second_nearest_point
+                    second_nearest_point = self.best_lap[2]
+            else:
+                if dist(current_x, current_y, self.best_lap[i].x, self.best_lap[i].y) <\
+                dist(current_x, current_y, nearest_point.x, nearest_point.y):
+                    third_nearest_point = second_nearest_point
+                    second_nearest_point = nearest_point
+                    nearest_point = self.best_lap[i]
+                elif dist(current_x, current_y, self.best_lap[i].x, self.best_lap[i].y) <\
+                dist(current_x, current_y, second_nearest_point.x, second_nearest_point.y):
+                    third_nearest_point = second_nearest_point
+                    second_nearest_point = self.best_lap[i]
+                elif dist(current_x, current_y, self.best_lap[i].x, self.best_lap[i].y) <\
+                dist(current_x, current_y, third_nearest_point.x, third_nearest_point.y):
+                    third_nearest_point = self.best_lap[i]
+        
+        # lines equations
+        def line1(x, y):
+            equation = (y - nearest_point.y)*(second_nearest_point.x - nearest_point.x)-(second_nearest_point.y-nearest_point.y)*(x-nearest_point.x)
+            A = (second_nearest_point.y - nearest_point.y) / (second_nearest_point.x - nearest_point.x)
+            C = (0 - nearest_point.y)*(second_nearest_point.x - nearest_point.x)-(second_nearest_point.y-nearest_point.y)*(0-nearest_point.x)
+            B = (equation - C - A*x)/y
+            return equation, A, B, C
+        def line2(x, y):
+            equation = (y - nearest_point.y)*(third_nearest_point.x - nearest_point.x)-(third_nearest_point.y-nearest_point.y)*(x-nearest_point.x)
+            A = (third_nearest_point.y - nearest_point.y) / (third_nearest_point.x - nearest_point.x)
+            C = (0 - nearest_point.y)*(third_nearest_point.x - nearest_point.x)-(third_nearest_point.y-nearest_point.y)*(0-nearest_point.x)
+            B = (equation - C - A*x)/y
+            return equation, A, B, C
+        
+        eq1, A1, B1, C1 = line1(current_x, current_y)
+        eq2, A2, B2, C2 = line2(current_x, current_y)
+
+        # finding proper distance
+        distance1 = abs(eq1)/sqrt(A1**2 + B1**2)
+        distance2 = abs(eq2)/sqrt(A2**2 + B2**2)
+
+        def perpendicular_line(A, x, y):
+            a_new_line = A ** (-1)
+            # Ax + By + C = 0
+            # y = ax + b => b = y - ax
+            b_new_line = y - A*x
+            return a_new_line, b_new_line
+
+        if distance1 < distance2:
+            a1, b1 = perpendicular_line(A1, current_x, current_y)
+            # fastest lap racing line
+            a2 = A1/B1
+            b2 = C1
+            points = '1-2'
+            lenght_best_lap = dist(nearest_point.x, nearest_point.y, second_nearest_point.x, second_nearest_point.y)
+        else:
+            a1, b1 = perpendicular_line(A2, current_x, current_y)
+            # fastest lap racing line
+            a2 = A2/B2
+            b2 = C2
+            points = '3-1'
+            lenght_best_lap = dist(third_nearest_point.x, third_nearest_point.y, nearest_point.x, nearest_point.y)
+
+        # finding intersection
+        x_inter = (b2 - b1)/(a1 - a2)
+        y_inter = a1*x_inter + b1
+
+        lenght_closest_point = dist(nearest_point.x, nearest_point.y, x_inter, y_inter)
+
+        if points == '1-2':
+            alfa = (lenght_best_lap - lenght_closest_point)/lenght_best_lap
+        else:
+            alfa = lenght_closest_point/lenght_best_lap
+
+        return alfa
+
+        
+
+        
 
 
 
 
-
-def calc_time_loss():
