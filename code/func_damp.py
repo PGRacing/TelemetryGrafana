620467ee9751db9d59a1993d353f6d839acbec23
Malwina796 <malwina@fastmail.com>
2024-04-18 20:42:00 +0200

    add calculating value range of dampers move

--- code/func_damp.py
+++ code/func_damp.py
@@ -58,7 +58,7 @@ f = list(KalmanFilter(dim_x=2, dim_z=1) for i in range(5))
 # 6  SW skret w prawo - spadek R
 
 
-def import_csv_damp(filepath, start_time):
+def import_csv_damp(filepath, start_time, time_coefficients):
     # CSV column names as following:
     # timestamp,ID,delta
     # date like '2023-11-04'
@@ -66,6 +66,8 @@ def import_csv_damp(filepath, start_time):
     csv_reader = csv.DictReader(file)
     line_count = 0
     points = []
+    
+    damp_data = DAMPKalman()
     #data_to_export = []
     startTime = datetime.datetime.now()
     previous_timestamp = None
@@ -74,30 +76,48 @@ def import_csv_damp(filepath, start_time):
     for row in csv_reader:
         # print(f'{row["timestamp"]}; {row["ID"]}; {row["delta"]}')
 
-        timestamp = start_time_add_timestamp(start_time, row["timestamp"])
+        #timestamp = start_time_add_timestamp(start_time, row["timestamp"])
 
-        #if line_count < 1:
-            #init_time = csv_timestamp_to_timedelta(row["timestamp"])
-            #first_timestamp = correct_init_time(init_time)
-            #timestamp = start_time + first_timestamp
-        #else:
-            #if len(time_coefficients) == 1:
-                #time_coefficient = 0.9489
-            #else:
-                #for i in range (1, len(time_coefficients)):
-                    #if csv_timestamp_to_timedelta(row["timestamp"]) >= csv_timestamp_to_timedelta(time_coefficients[i-1][0]) and \
-                    #csv_timestamp_to_timedelta(row["timestamp"]) < csv_timestamp_to_timedelta(time_coefficients[i][0]):
-                        #time_coefficient = time_coefficients[i-1][1]
-                    #elif csv_timestamp_to_timedelta(row["timestamp"]) <= csv_timestamp_to_timedelta(time_coefficients[0][0]):
-                        #time_coefficient = time_coefficients[0][1]
-                    #elif csv_timestamp_to_timedelta(row["timestamp"]) >= csv_timestamp_to_timedelta(time_coefficients[len(time_coefficients) - 1][0]):
-                        #time_coefficient = time_coefficients[len(time_coefficients) - 1][1]
+        if line_count < 1:
+            init_time = csv_timestamp_to_timedelta(row["timestamp"])
+            first_timestamp = correct_init_time(init_time)
+            timestamp = start_time + first_timestamp
+        else:
+            if len(time_coefficients) == 1:
+                time_coefficient = 0.9489
+            else:
+                for i in range (1, len(time_coefficients)):
+                    if csv_timestamp_to_timedelta(row["timestamp"]) >= csv_timestamp_to_timedelta(time_coefficients[i-1][0]) and \
+                    csv_timestamp_to_timedelta(row["timestamp"]) < csv_timestamp_to_timedelta(time_coefficients[i][0]):
+                        time_coefficient = time_coefficients[i-1][1]
+                    elif csv_timestamp_to_timedelta(row["timestamp"]) <= csv_timestamp_to_timedelta(time_coefficients[0][0]):
+                        time_coefficient = time_coefficients[0][1]
+                    elif csv_timestamp_to_timedelta(row["timestamp"]) >= csv_timestamp_to_timedelta(time_coefficients[len(time_coefficients) - 1][0]):
+                        time_coefficient = time_coefficients[len(time_coefficients) - 1][1]
 
-            #timestamp = correct_csv_timestamp(previous_csv_timestamp, row["timestamp"], previous_timestamp, time_coefficient)
+            timestamp = correct_csv_timestamp(previous_csv_timestamp, row["timestamp"], previous_timestamp, time_coefficient)
 
         if line_count == 0:
-            setup_kalman_filter()
-        data = filter_data(calc_wheel_position(row), row["ID"])
+            damp_data.init_kalman()
+        data = filter_data(calc_wheel_position(row), row["ID"], damp_data)
+        if row["ID"] != '6':
+            position = damp_data.find_velocity_range(data[1])
+            if position <= 0:
+                point = (
+                    Point('damp')
+                    .tag("ID", f'{row["ID"]}')
+                    .field("position_rebound", position)
+                    .time(timestamp)
+                )
+                points.append(point)
+            else:
+                point = (
+                    Point('damp')
+                    .tag("ID", f'{row["ID"]}')
+                    .field("position_bump", position)
+                    .time(timestamp)
+                )
+                points.append(point)
         #minutes_total = datetime.datetime(year=2023, month=11, day=5, hour=9, minute=54)
 
         #if timestamp > start_of_lap:
@@ -106,8 +126,11 @@ def import_csv_damp(filepath, start_time):
         #        time = time_s.total_seconds()
         #        data_to_export.append([time, data[0]])
 
-        #previous_timestamp = timestamp
-        #previous_csv_timestamp = row["timestamp"]
+        previous_timestamp = timestamp
+        previous_csv_timestamp = row["timestamp"]
+        
+
+
         point = (
             Point('damp')
             .tag("ID", f'{row["ID"]}')
@@ -129,7 +152,7 @@ def import_csv_damp(filepath, start_time):
             Point('damp')
             .tag("ID", f'{row["ID"]}')
             .field("raw_delta", float(int(row["delta"])))
-            .time(timestamp)
+            #.time(timestamp)
         )
         # points.append(point)
 
@@ -167,7 +190,7 @@ def setup_kalman_filter():
         f[i].Q = Q_discrete_white_noise(dim=2, dt=0.004, var=var_a)  # process noise
 
 
-def filter_data(data, ID):
+def filter_data(data, ID, damp_data):
     index = -1
     match int(ID):
         case 7:
@@ -182,10 +205,11 @@ def filter_data(data, ID):
             index = 4
 
     if index != -1:
-        f[index].predict()
-        f[index].update(data)
+        damp_data.filter_damp(index, data)
+        #f[index].predict()
+        #f[index].update(data)
         #print(f'{float(f[index].x[0][0])}, {float(f[index].x[1][0])}')
-        return float(f[index].x[0][0]), float(f[index].x[1][0])
+        return float(damp_data.f[index].x[0][0]), float(damp_data.f[index].x[1][0])
     else:
         return data, 0.0
 
@@ -231,3 +255,80 @@ def calc_wheel_position(row):
         position = delta
 
     return position
+
+
+class DAMPKalman:
+    def __init__(self) -> None:
+        self.f = list(range(5))
+        self.var = 0.5 * 530
+        self.var_damp = 0.014 
+        self.timestep = 0.004 
+        self.bump = [] 
+        self.rebound = []
+        self.damp_vel_percentage = []
+        for i in range(10, 80, 10):
+            self.bump.append([i, 0])
+        for i in range(-70, 10, 10):
+            self.rebound.append([i, 0])
+            #self.damp_vel_percentage.append([i, 0])
+
+    def init_kalman(self):
+        for i in range(5):
+            self.f[i] = KalmanFilter(dim_x=2, dim_z=1)
+            self.f[i].x = np.array([[0.], 
+                                    [0.]])  # initial state (position and velocity)
+            self.f[i].F = np.array([[1., self.timestep], 
+                                    [0., 1.]])  # state transition matrix
+            self.f[i].H = np.array([[1., 0.]])  # Measurement function
+            self.f[i].P = np.array([[10., 0.], 
+                                    [0., 10.]])  # covariance matrix
+            self.f[i].R = np.array([[self.var_damp]])  # measurement noise
+            self.f[i].Q = Q_discrete_white_noise(dim=2, dt=self.timestep, var=self.var)  # process noise
+
+    
+    def filter_damp(self, index:int, value:float):
+        self.f[index].predict()
+        self.f[index].update(value)
+
+    def find_velocity_range(self, velocity):
+        # ugina się to prędkość na +
+        # odgina to -
+        if velocity < 0:
+            for i in range(len(self.rebound)):
+                if (velocity <= self.rebound[i][0]):
+                    return self.rebound[i][0]
+        else:
+            for i in range(len(self.bump)):
+                if (velocity <= self.bump[i][0]):
+                    return self.bump[i][0]
+            return self.bump[i][0]
+
+    def calc_damp_values_percentage(self):
+        number_of_records = 0
+        for pair in self.bound:
+            number_of_records += pair[1]
+        
+
+'''
+path = 'C:/Users/malwi/Documents/MEGA/PGRacingTeam/000 telemetry_data/23-11-05 Pszczolki/damp/'       
+if __name__ == '__main__':
+    damp_data = DAMPKalman()
+    temp_timestamp = datetime.datetime(year = 2023, month=11, day=5, hour=11)
+    for item in os.listdir(path):
+        full_path = os.path.join(path, item)
+
+        if os.path.isfile(full_path) and item.endswith('.csv'):
+            import_csv_damp(full_path, 0, damp_data)
+            if item == 'DAMP0101-33.csv':
+                damp_data.calc_damp_values_percentage()
+                points = []
+                for i in range(len(damp_data.damp_vel_percentage)):
+                    point = (
+                        Point('damp')
+                        .tag("range", f'{damp_data.damp_vel_percentage[i][0]}%')
+                        .field("percentage", float(damp_data.damp_vel_percentage[i][1]))
+                        .time(temp_timestamp)
+                    )
+                    points.append(point)
+                write_api.write(bucket=bucket, org=org, record=points)
+'''
